e9ae801ab85d801e32cdd46e9b87739f339a3881
Blake Ingenthron <blakeingenthron@gmail.com>
2023-06-24 15:27:07 -0500

    initial stripe setup

---
+++ src/config/subscriptions.ts
@@ -0,0 +1,17 @@
+import type { SubscriptionPlan } from "@/types";
+
+export const freePlan: SubscriptionPlan = {
+  name: "Ollie",
+  description:
+    "The ollie plan allows you to create up to 1 store, and 20 products.",
+  stripePriceId: "",
+  monthlyPrice: null,
+};
+
+export const proPlan: SubscriptionPlan = {
+  name: "Kickflip",
+  description:
+    "The kickflip plan allows you to create up to 3 stores, and 20 products per store.",
+  stripePriceId: process.env.STRIPE_PRO_STORE_PRICE_ID ?? "",
+  monthlyPrice: 10,
+};
